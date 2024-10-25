83651a8e4d3f4223ef1bae419106e747f061d077
Samandar <msamandar201@gmail.com>
2024-10-25 15:51:25 +0500

    try add models store

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['name', 'descriptions',]
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display=['name', 'category',]
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display=['name', 'price',]
+
--- store/models.py
+++ store/models.py
@@ -4,12 +4,42 @@ from django.db import models
 
 #Asilbek, Samandar, Bobur
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    descriptions = models.TextField()
+    adress = models.TextField()
+    phone_number = models.IntegerField()
+    rating = models.FloatField()
+    image = models.ImageField()
+    delivery_time = models.DateField()
+    working_time = models.DateField()
+    categoty_type = models.CharField(max_length=500)
+    
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categorys'
 
 
 class Subcategory(models.Model):
-    pass
+    description = models.TextField()
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, models.CASCADE)
+    image = models.ImageField()
+    
+    class Meta:
+        verbose_name = 'Subcategory'
+        verbose_name_plural = 'Subcategorys'
 
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=500)
+    descriptions = models.TextField()
+    price = models.IntegerField()
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE)
+    image = models.ImageField()
+    stock = models.CharField(max_length=500)
+    
+    
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
