4d79b9529b89b2872cfa94b0cedd385d82c0f358
Aminjon33 <usmonov440man@gmail.com>
2024-10-28 21:38:01 +0500

    store

--- store/views.py
+++ store/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.views import View
 from store.models import Category
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
 from django.http import JsonResponse
+import json
 
 
 
@@ -44,3 +47,71 @@ class StoreListAPIView(View):
     #post
     
 #postman
+
+class StoreDeleteView(View):
+    
+    def delete(self, request, id):
+        store = get_object_or_404(Category, Q(id=id) & Q(category_type='Store'))
+        store_name = self.name
+        store.delete()
+        json_response = {
+            'message': f'Store "{store_name}" has been successfully deleted.'
+        }
+        return JsonResponse(json_response, status=200)
+    
+
+class StoreUpdateView(View):
+    def put(self, request, id):
+        store = get_object_or_404(Category, Q(id=id) & Q(category_type='Store'))
+        data = json.loads(request.body)
+
+        if 'name' in data:
+            store.name = data['name']
+        if 'description' in data:
+            store.description = data['description']
+        if 'phone_number' in data:
+            store.phone_number = data['phone_number']
+        if 'rating' in data:
+            store.rating = data['rating']
+        if 'address' in data:
+            if 'city' in data['address']:
+                store.address.city = data['address']['city']
+            if 'district' in data['address']:
+                store.address.district = data['address']['district']
+
+                store.save()
+        
+        json_response = {
+            'message': f'Store "{store.name}" has been successfully updated.'
+        }
+        
+        return JsonResponse(json_response, status=200)
+    
+class StorePartialUpdateView(View):
+
+    def patch(self, request, id):
+        store = get_object_or_404(Category, Q(id=id) & Q(category_type='Store'))
+
+        data = json.loads(request.body)
+        
+        if 'name' in data:
+            store.name = data['name']
+        if 'description' in data:
+            store.description = data['description']
+        if 'phone_number' in data:
+            store.phone_number = data['phone_number']
+        if 'rating' in data:
+            store.rating = data['rating']
+        if 'address' in data:
+            if 'city' in data['address']:
+                store.address.city = data['address']['city']
+            if 'district' in data['address']:
+                store.address.district = data['address']['district']
+
+        store.save()
+        
+        json_response = {
+            'message': f'Store "{store.name}" has been partially updated.'
+        }
+        
+        return JsonResponse(json_response, status=200)
