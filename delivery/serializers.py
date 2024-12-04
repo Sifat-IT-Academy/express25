713e315e812145e627699cd280c76c87528b3d95
muslimbek77 <muslimjonbekali@mail.ru>
2024-12-04 16:30:07 +0500

    fix code

--- delivery/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Delivery
-
-class DeliverySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Delivery
-        fields = '__all__' 
