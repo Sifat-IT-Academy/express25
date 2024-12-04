afd1bab342bc4702263f6cfb47bd8c4d6b591d9d
muslimbek77 <muslimjonbekali@mail.ru>
2024-12-04 16:35:32 +0500

    delivery and order fix code

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,12 +1,4 @@
-# from django.urls import path
-# from . import views
+from django.urls import path
+urlpatterns = [
+]
 
-# urlpatterns = [
-#     # Delivery views
-#     path('', views.delivery_list, name='delivery_list'),
-#     path('delivery/<int:delivery_id>/', views.delivery_detail, name='delivery_detail'),
-#     path('delivery/<int:delivery_id>/update-status/', views.delivery_status_update, name='delivery_status_update'),
-    
-#     # Review views
-#     path('delivery/<int:delivery_id>/add-review/', views.review_create, name='review_create'),
-# ]
--- order/urls.py
+++ order/urls.py
@@ -1,27 +1,4 @@
-# from django.urls import path
-# from .views import (
-#     OrderListView, OrderDetailView, OrderDeleteView,
-#     OrderUpdateView, OrderPartialUpdateView, PaymentCreateView
-# )
-
-# urlpatterns = [
-#     path('orders/', OrderListView.as_view(), name='order_list'),
-#     path('orders/<int:id>/', OrderDetailView.as_view(), name='order_detail'),
-#     path('orders/<int:id>/delete/', OrderDeleteView.as_view(), name='order_delete'),
-#     path('orders/<int:id>/update/', OrderUpdateView.as_view(), name='order_update'),
-#     path('orders/<int:id>/partial-update/', OrderPartialUpdateView.as_view(), name='order_partial_update'),
-#     path('payments/create/', PaymentCreateView.as_view(), name='payment_create'),
-# ]
-
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import OrderViewSet, PaymentViewSet
-
-
-router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='order')
-router.register(r'payments', PaymentViewSet, basename='payment')
+from django.urls import path
 urlpatterns = [
-    path('', include(router.urls)),
 ]
 
