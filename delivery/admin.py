67e0943c7e2cd81ad855839efdb9e82cf5a52427
muslimbek77 <muslimjonbekali@mail.ru>
2024-12-04 16:13:05 +0500

    delivery and order

--- delivery/admin.py
+++ delivery/admin.py
@@ -4,16 +4,15 @@ from .models import Delivery, Review
 
 @admin.register(Delivery)
 class DeliveryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'order', 'courier', 'delivery_time', 'delivered_at', 'status', 'average_rating')
-    list_filter = ('status',)
+    list_display = ('id', 'order', 'courier', 'delivery_time', 'delivered_at', 'status', 'get_average_rating')
+    list_filter = ('status', 'delivery_time')
     search_fields = ('order', 'courier')
     ordering = ('-delivered_at',)
-    readonly_fields = ('average_rating',)
+    readonly_fields = ('get_average_rating',)
 
-    def average_rating(self, obj):
+    def get_average_rating(self, obj):
         return obj.average_rating() or "No Reviews"
-    average_rating.short_description = "Average Rating"
-
+    get_average_rating.short_description = "Average Rating"
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
@@ -21,3 +20,4 @@ class ReviewAdmin(admin.ModelAdmin):
     list_filter = ('rating',)
     search_fields = ('customer_name', 'store_name', 'delivery__order')
     ordering = ('-created_at',)
+    autocomplete_fields = ('delivery',)
--- order/admin.py
+++ order/admin.py
@@ -1,22 +1,29 @@
 from django.contrib import admin
 from .models import Order, ProductOrder, Payment
 
-
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'customer', 'courier', 'status', 'total_price', 'created_at', 'updated_at')  
-    list_filter = ('status', 'created_at') 
+    list_display = ('id', 'customer', 'courier', 'status', 'total_price', 'created_at', 'updated_at')
+    list_filter = ('status', 'created_at', 'updated_at')
     search_fields = ('customer__username', 'courier__username')
+    ordering = ('-created_at',)
+    readonly_fields = ('created_at', 'updated_at')
+    autocomplete_fields = ('customer', 'courier')
+
 
 @admin.register(ProductOrder)
 class ProductOrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'order', 'product', 'quantity')
-    list_filter = ('order', 'product')  
-    search_fields = ('order__id', 'product__name')  
+    list_filter = ('order', 'product')
+    search_fields = ('order__id', 'product__name')
+    ordering = ('order',)
+    autocomplete_fields = ('order', 'product')
+
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'order', 'amount', 'method', 'status')  
-    list_filter = ('status', 'method')  
-    search_fields = ('order__id', 'amount') 
-    ordering = ('-status',)  
+    list_display = ('id', 'order', 'amount', 'method', 'status')
+    list_filter = ('status', 'method')
+    search_fields = ('order__id', 'amount')
+    ordering = ('-id',)
+    autocomplete_fields = ('order',)
