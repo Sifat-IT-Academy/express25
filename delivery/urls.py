babbb0ecad72c05d9877496d78790b11d653bf9e
Web-Developer-back-end <sharapovashaxnoza03@gmail.com>
2024-12-11 14:24:00 +0500

    delivery urls

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
+from delivery.views import (
+    DeliveryListView,
+    DeliveryStatusView,
+    ReviewCreateView,
+    CreateDeliveryView,
+)
+
 urlpatterns = [
-
+    path('deliveries/', DeliveryListView.as_view(), name='delivery-list'),  
+    path('deliveries/<int:pk>/', DeliveryStatusView.as_view(), name='delivery-detail'),
+    path('deliveries/create/', CreateDeliveryView.as_view(), name='create-delivery'), 
+    path('reviews/', ReviewCreateView.as_view(), name='review-create'),  
 ]
-
